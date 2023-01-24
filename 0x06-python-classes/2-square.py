77a0ec4170e4c28b26dcbeee7c62b398d9b6b295
regina <geenaa21@gmail.com>
2023-01-24 13:55:10 +0100

    objects and classes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 
 """Define a class Square."""
@@ -17,3 +16,4 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+        
