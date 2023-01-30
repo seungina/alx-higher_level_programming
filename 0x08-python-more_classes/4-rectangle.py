4b71d3c76f93997f9b68768f74091fb2873863b1
regina <geenaa21@gmail.com>
2023-01-30 23:20:21 +0100

    more classes

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -61,4 +61,4 @@ class Rectangle:
 
     def __repr__(self):
         """returns a string representation of the rectangle for reproduction"""
-        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)        
